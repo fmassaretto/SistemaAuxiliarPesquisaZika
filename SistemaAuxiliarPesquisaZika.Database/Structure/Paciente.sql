d9c311a2d2d1c27842af792032963a5441b3974d
fmassaretto <fmassaretto@WKSJUN000494>
2017-04-25 16:48:13 -0300

    Some database files

---
+++ SistemaAuxiliarPesquisaZika.Database/Structure/Paciente.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Paciente]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [NumeroCaso] INT NOT NULL, 
+    [NomeCompleto] VARCHAR(200) NOT NULL, 
+    [CPF] VARCHAR(50) NULL, 
+    [RG] VARCHAR(50) NULL, 
+    [Endereco] VARCHAR(200) NULL, 
+    [Bairro] VARCHAR(50) NULL, 
+    [Cidade] VARCHAR(50) NULL, 
+    [UBSFrequenta] VARCHAR(100) NULL, 
+    [Email] VARCHAR(100) NULL, 
+    [Telefone] VARCHAR(50) NULL, 
+    [DataNascimento] DATETIME NULL
+)
