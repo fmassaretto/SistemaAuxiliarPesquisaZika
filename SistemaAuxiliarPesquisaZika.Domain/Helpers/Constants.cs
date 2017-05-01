449bd63ad00fb7a904a0f5c83a4a4446b876f1a7
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-05-01 01:48:45 -0300

    Some helpers to access the project constants

---
+++ SistemaAuxiliarPesquisaZika.Domain/Helpers/Constants.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace SistemaAuxiliarPesquisaZika.Domain.Helpers
+{
+    public class Constants
+    {
+        
+        private static List<ReagenteNaoReagente> listaReagenteNaoReagente;
+
+        public static List<ReagenteNaoReagente> ListaReagenteNaoReagente
+        {
+            get
+            {               
+                return ListaReagenteNãoReagente();
+            }
+        }
+
+        public static List<ReagenteNaoReagente> ListaReagenteNãoReagente()
+        {
+            ReagenteNaoReagente OReagenteNaoReagente = new ReagenteNaoReagente();
+            if (listaReagenteNaoReagente == null)
+            {
+                listaReagenteNaoReagente.Add(OReagenteNaoReagente.Id = 0);
+                listaReagenteNaoReagente.Add(new ReagenteNaoReagente()
+                {
+                    Id = 1,
+                    Value = "Reagente"
+                });
+                listaReagenteNaoReagente.Add(new ReagenteNaoReagente()
+                {
+                    Id = 2,
+                    Value = "Não Reagente / Imune"
+                });
+            }
+            return listaReagenteNaoReagente;
+        }
+
+        private static List<string> positivoNegativo;
+
+        public static List<string> PositivoNegativo
+        {
+            get
+            {
+                positivoNegativo.Add("");
+                positivoNegativo.Add("Positivo");
+                positivoNegativo.Add("Negativo");
+                return positivoNegativo;
+            }
+        }
+
+        public Constants()
+        {
+            listaReagenteNaoReagente = new List<ReagenteNaoReagente>();
+            ListaReagenteNãoReagente();
+        }
+
+    }
+}
---
+++ SistemaAuxiliarPesquisaZika.Domain/Helpers/ReagenteNaoReagente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAuxiliarPesquisaZika.Domain.Helpers
+{
+    public class ReagenteNaoReagente
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
