21ab7164f885eb1e8cabb6bd850fa5a9ad4dcf1e
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-05-06 23:45:26 -0300

    Add startup.cs for site security to work with Request.IsAuthenticated

--- SistemaAuxiliarPesquisaZika.WebASPNET/App_Start/Startup.cs
+++ SistemaAuxiliarPesquisaZika.WebASPNET/App_Start/Startup.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
 
 [assembly: OwinStartup(typeof(SistemaAuxiliarPesquisaZika.WebASPNET.App_Start.Startup))]
 
@@ -12,6 +14,11 @@ namespace SistemaAuxiliarPesquisaZika.WebASPNET.App_Start
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Usuario/Login")
+            });
         }
     }
 }
