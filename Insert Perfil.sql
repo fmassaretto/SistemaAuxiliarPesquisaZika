b3e04100f6771768ccb4c4ceb9f2267bb2d73364
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-05-08 22:41:05 -0300

    Added sql script to insert perfil

---
+++ Insert Perfil.sql
@@ -0,0 +1,22 @@
+USE [AuxSystemResearchZikaDB]
+GO
+
+INSERT INTO [dbo].[Perfil]
+           ([Nome])
+		VALUES
+			('Administrador')
+			INSERT INTO [dbo].[Perfil]
+			([Nome])
+		VALUES
+			('Pesquisador')
+			INSERT INTO [dbo].[Perfil]
+			([Nome])
+		VALUES
+			('Médico')
+			INSERT INTO [dbo].[Perfil]
+			([Nome])
+		VALUES
+			('Preenchedor')
+GO
+
+
