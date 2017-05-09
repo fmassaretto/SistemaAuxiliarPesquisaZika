5b187fcabbcd71d8d07da5870bc7f34bfcf83a03
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-05-08 22:35:43 -0300

    Added method GetExameByIdPaciente

--- SistemaAuxiliarPesquisaZika.Bussiness/ExameBSN.cs
+++ SistemaAuxiliarPesquisaZika.Bussiness/ExameBSN.cs
@@ -1,5 +1,6 @@
 ﻿using SistemaAuxiliarPesquisaZika.Bussiness.Abstract;
 using SistemaAuxiliarPesquisaZika.Data.Context;
+using SistemaAuxiliarPesquisaZika.Domain;
 using SistemaAuxiliarPesquisaZika.Domain.DTO;
 using System;
 using System.Collections.Generic;
@@ -22,7 +23,21 @@ namespace SistemaAuxiliarPesquisaZika.Bussiness
                                    ExamesPaciente = SelectAll
                                }).ToList();
             return resultQuery;
+        }
 
+        //TODO: Ideal seria fazer um IEnumerable pois o paciente pode ter mais de um exame,
+        // mas por motivo de facilidade só estou passando uma ViewModel (PacienteExameViewModel)
+        //Atensão lembrar de mudar o @model da view
+        public PacienteExameViewModel GetExameByIdPaciente(int? idPaciente)
+        {
+            var resultExame = (from p in _db.Paciente
+                               join ep in _db.ExamesPaciente on p.Id equals ep.IdPaciente
+                               where p.Id == idPaciente
+                               select new PacienteExameViewModel{
+                                   Paciente = p,
+                                   ExamesPaciente = ep
+                               }).FirstOrDefault();
+            return resultExame;
         }
 
         public void Dispose()
