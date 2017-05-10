378ebe403d31f733f95bf05334307742538711ca
fmassaretto <fmassaretto@WKSJUN000494>
2017-05-10 11:55:57 -0300

    Add seed Perfil to fill automatic the table

--- SistemaAuxiliarPesquisaZika.Data/Migrations/Configuration.cs
+++ SistemaAuxiliarPesquisaZika.Data/Migrations/Configuration.cs
@@ -15,12 +15,20 @@ namespace SistemaAuxiliarPesquisaZika.Data.Migrations
 
         protected override void Seed(AuxSystemResearchContext context)
         {
-            //context.Perfil.AddOrUpdate(
-            //    p => p.Nome,
-            //    new Perfil { Nome = "Administrador" },
-            //    new Perfil { Nome = "Pesquisador" },
-            //    new Perfil { Nome = "Médico" },
-            //    new Perfil { Nome = "Preenchedor" }
+            context.Perfil.AddOrUpdate(
+                p => p.Id,
+                new Perfil() { Id = 1, Nome = "Administrador" },
+                new Perfil() { Id = 2, Nome = "Pesquisador" },
+                new Perfil() { Id = 3, Nome = "Médico" },
+                new Perfil() { Id = 4, Nome = "Preenchedor" }
+                );
+
+            //context.Usuarios.AddOrUpdate(
+            //    u => u.Id,
+            //    new Usuario() { Id = 1, Nome = "Admin", Email = "admin@g.com", Senha = "123123", ConfirmaSenha = "123123", Ativo = true, IdPerfil = 1 },
+            //    new Usuario() { Id = 2, Nome = "João (Pesquisador)", Email = "joao@g.com", Senha = "123123", ConfirmaSenha = "123123", Ativo = true, IdPerfil = 2 },
+            //    new Usuario() { Id = 3, Nome = "Pedro (Médico)", Email = "pedro@g.com", Senha = "123123", ConfirmaSenha = "123123", Ativo = true, IdPerfil = 3 },
+            //    new Usuario() { Id = 4, Nome = "Ana (Preenchedor)", Email = "ana@g.com", Senha = "123123", ConfirmaSenha = "123123", Ativo = true, IdPerfil = 4 }
             //    );
             //  This method will be called after migrating to the latest version.
 
