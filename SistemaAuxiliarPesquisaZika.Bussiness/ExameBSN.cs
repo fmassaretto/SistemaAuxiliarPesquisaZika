4f1f57a4f952764a98bd71639788aec1085ba02e
fmassaretto <fmassaretto@WKSJUN000494>
2017-05-10 11:53:34 -0300

    Added  ConsultaRNComExame() and GetExameByIdRN(int? idRecemNascido)

--- SistemaAuxiliarPesquisaZika.Bussiness/ExameBSN.cs
+++ SistemaAuxiliarPesquisaZika.Bussiness/ExameBSN.cs
@@ -27,7 +27,7 @@ namespace SistemaAuxiliarPesquisaZika.Bussiness
 
         //TODO: Ideal seria fazer um IEnumerable pois o paciente pode ter mais de um exame,
         // mas por motivo de facilidade só estou passando uma ViewModel (PacienteExameViewModel)
-        //Atensão lembrar de mudar o @model da view
+        //Atenção lembrar de mudar o @model da view
         public PacienteExameViewModel GetExameByIdPaciente(int? idPaciente)
         {
             var resultExame = (from p in _db.Paciente
@@ -40,9 +40,39 @@ namespace SistemaAuxiliarPesquisaZika.Bussiness
             return resultExame;
         }
 
+        public IEnumerable<RNExameViewModel> ConsultaRNComExame()
+        {
+            var resultQuery = (from rn in _db.RecemNascido
+                               join e in _db.ExamesRecemNascido on rn.Id equals e.Id into AllColumns
+                               from SelectAll in AllColumns
+                               select new RNExameViewModel()
+                               {
+                                   RecemNascido = rn,
+                                   ExamesRecemNascido = SelectAll
+                               }).ToList();
+            return resultQuery;
+        }
+
+        //TODO: Ideal seria fazer um IEnumerable pois o recém-nascido pode ter mais de um exame,
+        // mas por motivo de facilidade só estou passando uma ViewModel (RNExameViewModel)
+        //Atenção lembrar de mudar o @model da view
+        public RNExameViewModel GetExameByIdRN(int? idRecemNascido)
+        {
+            var resultExame = (from rn in _db.RecemNascido
+                               join e in _db.ExamesRecemNascido on rn.Id equals e.Id
+                               where rn.Id == idRecemNascido
+                               select new RNExameViewModel
+                               {
+                                   RecemNascido = rn,
+                                   ExamesRecemNascido = e
+                               }).FirstOrDefault();
+            return resultExame;
+        }
+
         public void Dispose()
         {
             _db.Dispose();
+            GC.SuppressFinalize(this);
         }
     }
 }
