43f70144d04fff746c635b7641ece7b6fd7fe1d2
fmassaretto <fmassaretto@WKSJUN000494>
2017-05-11 11:48:35 -0300

    Added DTO RelatorioPaciente

---
+++ SistemaAuxiliarPesquisaZika.Domain/DTO/RelatorioPaciente.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaAuxiliarPesquisaZika.Domain.DTO
+{
+    public class RelatorioPaciente
+    {
+        public Paciente Paciente;
+        public PesquisaSocioSaude PesquisaSocioSaude;
+        public ExamesPaciente ExamesPaciente;
+    }
+}
