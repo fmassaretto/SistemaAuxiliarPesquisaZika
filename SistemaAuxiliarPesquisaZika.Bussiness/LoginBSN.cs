39edbf923622168c7ebdc12938035b47b782f22e
fmassaretto <fmassaretto@WKSJUN000494>
2017-05-11 11:47:36 -0300

    Try catch in LoginBSN and added RelatorioBSN

--- SistemaAuxiliarPesquisaZika.Bussiness/LoginBSN.cs
+++ SistemaAuxiliarPesquisaZika.Bussiness/LoginBSN.cs
@@ -13,30 +13,37 @@ namespace SistemaAuxiliarPesquisaZika.Bussiness
         {
             //senha = senha.GerarHash();
 
-            var retorno = (from u in _db.Usuarios
-                           join p in _db.Perfil on u.IdPerfil equals p.Id
-                           where (u.Email == email) && (u.Senha == senha) && (u.Ativo == true)
-                           select new {
-                               Id = u.Id,
-                               Nome = u.Nome,
-                               Email = u.Email,
-                               Ativo = u.Ativo,
-                               IdPerfil = p.Id,
-                               NomePerfil = p.Nome }).ToList();
-
-            
-
-            foreach (var item in retorno)
+            try
             {
-                usuario = new Usuario()
+                var retorno = (from u in _db.Usuarios
+                               join p in _db.Perfil on u.IdPerfil equals p.Id
+                               where (u.Email == email) && (u.Senha == senha) && (u.Ativo == true)
+                               select new
+                               {
+                                   Id = u.Id,
+                                   Nome = u.Nome,
+                                   Email = u.Email,
+                                   Ativo = u.Ativo,
+                                   IdPerfil = p.Id,
+                                   NomePerfil = p.Nome
+                               }).ToList();
+
+                foreach (var item in retorno)
                 {
-                    Id = item.Id,
-                    Nome = item.Nome,
-                    Email = item.Email,
-                    Ativo = item.Ativo,
-                    IdPerfil = item.IdPerfil,
-                    NomePerfil = item.NomePerfil
-                };
+                    usuario = new Usuario()
+                    {
+                        Id = item.Id,
+                        Nome = item.Nome,
+                        Email = item.Email,
+                        Ativo = item.Ativo,
+                        IdPerfil = item.IdPerfil,
+                        NomePerfil = item.NomePerfil
+                    };
+                }
+            }
+            catch (System.Data.SqlClient.SqlException SqlException)
+            {
+                throw new Exception("Problemas com a conexão com banco de dados", SqlException);
             }
             //IEnumerable<Usuario> retornoViewModel = retorno.Select(x => new Usuario(x.Id, x.Nome, x.Email, x.Ativo, new Perfil { Id = x.IdPerfil, Nome = x.PNome })
             //);
---
+++ SistemaAuxiliarPesquisaZika.Bussiness/RelatorioBSN.cs
@@ -0,0 +1,34 @@
+﻿using SistemaAuxiliarPesquisaZika.Bussiness.Abstract;
+using SistemaAuxiliarPesquisaZika.Data.Context;
+using SistemaAuxiliarPesquisaZika.Domain.DTO;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace SistemaAuxiliarPesquisaZika.Bussiness
+{
+    public class RelatorioBSN : RepositoryBSN<RelatorioPaciente>, IDisposable
+    {
+        private AuxSystemResearchContext _db = new AuxSystemResearchContext();
+
+        public IEnumerable<RelatorioPaciente> GerarRelatorioTudo()
+        {
+            var result = from p in _db.Paciente
+                         join ps in _db.PesquisaSocioSaude on p.Id equals ps.IdPaciente
+                         join ep in _db.ExamesPaciente on p.Id equals ep.IdPaciente
+                         select new RelatorioPaciente()
+                         {
+                             Paciente = p,
+                             PesquisaSocioSaude = ps,
+                             ExamesPaciente = ep
+                         };
+            return result;
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
