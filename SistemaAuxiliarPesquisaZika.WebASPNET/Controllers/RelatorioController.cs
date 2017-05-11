ee73e2f3e18acbc114679ef62a1cbe29966aef53
fmassaretto <fmassaretto@WKSJUN000494>
2017-05-11 11:50:56 -0300

    Removing unnecessary usings and added Relatorio Controller

---
+++ SistemaAuxiliarPesquisaZika.WebASPNET/Controllers/RelatorioController.cs
@@ -0,0 +1,15 @@
+﻿using SistemaAuxiliarPesquisaZika.Bussiness;
+using System.Web.Mvc;
+
+namespace SistemaAuxiliarPesquisaZika.WebASPNET.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        private RelatorioBSN _relatorioRepository = new RelatorioBSN();
+        public ActionResult RelatorioAllDataPaciente()
+        {
+            var relatorio = _relatorioRepository.GerarRelatorioTudo();
+            return View(relatorio);
+        }
+    }
+}
