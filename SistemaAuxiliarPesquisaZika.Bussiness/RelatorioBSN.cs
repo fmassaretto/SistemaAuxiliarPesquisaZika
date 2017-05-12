940da9a1037b5916d9c2533b834191bb9b112d7e
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-05-11 22:45:16 -0300

    Added GerarRelatorio and create csv file

--- SistemaAuxiliarPesquisaZika.Bussiness/RelatorioBSN.cs
+++ SistemaAuxiliarPesquisaZika.Bussiness/RelatorioBSN.cs
@@ -4,6 +4,10 @@ using SistemaAuxiliarPesquisaZika.Domain.DTO;
 using System.Linq;
 using System.Collections.Generic;
 using System;
+using System.IO;
+using System.Windows.Forms;
+using FileHelpers;
+using static System.DateTime;
 
 namespace SistemaAuxiliarPesquisaZika.Bussiness
 {
@@ -11,7 +15,7 @@ namespace SistemaAuxiliarPesquisaZika.Bussiness
     {
         private AuxSystemResearchContext _db = new AuxSystemResearchContext();
 
-        public IEnumerable<RelatorioPaciente> GerarRelatorioTudo()
+        public IEnumerable<RelatorioPaciente> SelectAllPaciente()
         {
             var result = from p in _db.Paciente
                          join ps in _db.PesquisaSocioSaude on p.Id equals ps.IdPaciente
@@ -23,6 +27,59 @@ namespace SistemaAuxiliarPesquisaZika.Bussiness
                              ExamesPaciente = ep
                          };
             return result;
+        }
+
+        public void GerarRelatorio(string nomeRelatorio)
+        {
+            //var engine = new FileHelperAsyncEngine<RelatorioPaciente>();
+            DateTime date = DateTime.Now;
+            var today = date.ToString("dd-MM-yyyy");
+            //string path = Path.Combine(Application.StartupPath, "Relatorios", nomeRelatorio);
+            string rootPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName;
+
+            if (!Directory.Exists(rootPath))
+            {
+                if (rootPath != null) Directory.CreateDirectory(rootPath);
+            }
+
+            string fullPath = Path.Combine(@"E:\", nomeRelatorio + today + ".csv");
+            if (rootPath != null)
+            {
+
+                if (!File.Exists(fullPath))
+                {
+                    File.Create(fullPath);
+                }
+            }
+
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(fullPath, false);               
+            }
+            catch(Exception)
+            {
+                sw.Close();
+            }
+
+            //engine.BeginWriteFile("");
+
+            var sap = SelectAllPaciente();
+
+            foreach (var result in sap)
+            {
+                var resPaciente = result.Paciente;
+                var resPesquisa = result.PesquisaSocioSaude;
+                var resExame = result.ExamesPaciente;
+                sw.Write(resPaciente.NumeroCaso);
+                sw.Write(resPaciente.NomeCompleto);
+                sw.Write(resPesquisa.VivePaiRN);
+                sw.Write(resPesquisa.TrabalhoRemunerado);
+                sw.Write(resExame.ResultadoHB);
+                sw.Write(resExame.ResultadoHT);
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
         }
 
         public void Dispose()
