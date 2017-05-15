8e5c2ab3227e26e0178573799f6fda7899e92b55
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-05-14 23:46:36 -0300

    Removed unused usings

--- SistemaAuxiliarPesquisaZika.WebASPNET/ViewModels/UsuariosViewModel.cs
+++ SistemaAuxiliarPesquisaZika.WebASPNET/ViewModels/UsuariosViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using SistemaAuxiliarPesquisaZika.Domain;
-using SistemaAuxiliarPesquisaZika.Domain.DTO;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
 namespace SistemaAuxiliarPesquisaZika.WebASPNET.ViewModels
