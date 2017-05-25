d53a8a62cf69f3a0750ac15ff8c7b7538d64f4ed
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-05-24 22:54:14 -0300

    Created twilio SMS classes and interfaces; Add keys(SID, Token, phonenumber) in appSetting in App.config

---
+++ ZikaVirusProject.Services.TwilioSMS/Interface/IRestClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace ZikaVirusProject.Services.TwilioSMS.Interface
+{
+    public interface IRestClient
+    {
+        Task<MessageResource> SendMessage(string from, string to, string body, List<Uri> mediaUrl);
+    }
+}
---
+++ ZikaVirusProject.Services.TwilioSMS/RestClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using ZikaVirusProject.Services.TwilioSMS.Interface;
+
+namespace ZikaVirusProject.Services.TwilioSMS
+{
+    public class RestClient : IRestClient
+    {
+        private readonly ITwilioRestClient _client;
+
+        public RestClient()
+        {
+            _client = new TwilioRestClient(
+                Credentials.TwilioAccountSid,
+                Credentials.TwilioAuthToken
+            );
+        }
+
+        public RestClient(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+        public async Task<MessageResource> SendMessage(string @from, string to, string body, List<Uri> mediaUrl)
+        {
+            var toPhoneNumber = new PhoneNumber(to);
+            return await MessageResource.CreateAsync(
+                toPhoneNumber,
+                @from: new PhoneNumber(from),
+                body: body,
+                mediaUrl: mediaUrl,
+                client: _client);
+        }
+    }
+}
