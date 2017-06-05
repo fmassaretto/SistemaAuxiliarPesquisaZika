ef77d2d7d725ed370fd00aa7e0b26b6d01fc3a44
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-06-04 23:09:33 -0300

    Use object initializer

--- ZikaVirusProject.Services/Email/Email.cs
+++ ZikaVirusProject.Services/Email/Email.cs
@@ -6,10 +6,12 @@ namespace ZikaVirusProject.Services.Email
     {
         public Email()
         {
-            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
-            client.Host = "smtp.gmail.com";
-            client.EnableSsl = true;
-            client.Credentials = new System.Net.NetworkCredential("seu email", "sua senha");
+            SmtpClient client = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                EnableSsl = true,
+                Credentials = new System.Net.NetworkCredential("seu email", "sua senha")
+            };
             MailMessage mail = new MailMessage();
             mail.Sender = new System.Net.Mail.MailAddress("email que vai enviar", "Remetente");
             mail.From = new MailAddress("de quem", "Remetente");
