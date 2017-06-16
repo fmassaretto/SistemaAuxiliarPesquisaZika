98fa22a91b329da696a72c3226ee2c4f34ac6128
FABIO MASSARETTO <fabio.massaretto01@fatec.sp.gov.br>
2017-06-16 16:23:52 -0300

    Created Usuarios controller and view

---
+++ SistemaAuxiliarPesquisaZika.WebASPNET/Controllers/UsuariosController.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using SistemaAuxiliarPesquisaZika.Bussiness;
+using SistemaAuxiliarPesquisaZika.Data.Context;
+using SistemaAuxiliarPesquisaZika.Domain;
+using SistemaAuxiliarPesquisaZika.Domain.DTO;
+using EntityState = System.Data.Entity.EntityState;
+
+namespace SistemaAuxiliarPesquisaZika.WebASPNET.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private AuxSystemResearchContext db = new AuxSystemResearchContext();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            var usuarios = db.Usuarios.Include(u => u.Perfil);
+            return View(usuarios.ToList());
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.Perfil, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Email,Senha,ConfirmaSenha,Ativo,IdPerfil")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.Perfil, "Id", "Nome", usuario.Id);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.Perfil, "Id", "Nome", usuario.Id);
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Email,Senha,ConfirmaSenha,Ativo,IdPerfil")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.Perfil, "Id", "Nome", usuario.Id);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuario usuario = db.Usuarios.Find(id);
+            db.Usuarios.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            Usuario usuario;
+
+            using (LoginBSN repo = new LoginBSN())
+            {
+                usuario = repo.Login(model.Email, model.Senha);
+
+            }
+
+            // Login com sucesso
+            if (usuario != null)
+            {
+
+                var ident = new ClaimsIdentity(
+                    new[]
+                    {
+                        // Padrão para utilzar o mecanismo antiforgery
+                        new Claim(ClaimTypes.NameIdentifier, usuario.Id.ToString()),
+                        new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider",
+                            "ASP.NET Identity", "http://www.w3.org/2001/XMLSchema#string"),
+
+                        // Outras Claims
+
+                        // Nome do Usuario
+                        new Claim(ClaimTypes.Name, usuario.Nome),
+
+                        // Perfil
+                        new Claim(ClaimTypes.Role, usuario.NomePerfil),
+
+                        // Email
+                        new Claim(ClaimTypes.Email, usuario.Email)
+
+                    },
+                    DefaultAuthenticationTypes.ApplicationCookie);
+
+                HttpContext.GetOwinContext().Authentication.SignIn(
+                    new AuthenticationProperties { IsPersistent = false }, ident);
+
+                //Usuario Usuario = new Usuario();
+                //int user = usuario.Id;
+                //Usuario.Id = usuario.Id;
+                //ViewData["IdUsuario"] = user;
+
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                return RedirectToAction($"Index", $"Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Credenciais Inválidas.");
+                return View(model);
+            }
+        }
+
+        public ActionResult LogOff()
+        {
+            var authenticationManager = HttpContext.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            Session.Abandon();
+            return RedirectToAction($"Index", $"Home");
+        }
+
+        public ActionResult Registrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
